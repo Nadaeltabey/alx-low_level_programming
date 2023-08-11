66a73f065f56e98b6ff0823d5dcc9270b6309c52
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-10 18:01:07 -0700

    0c6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 
 printf("Size of a char: %u byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of an int: %u byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a long int: %u byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %u byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a long long int: %u byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %u byte(s)\n", (unsigned long)sizeof(i));
 return (0);
