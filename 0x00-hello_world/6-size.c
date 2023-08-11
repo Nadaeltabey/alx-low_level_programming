334de2b20a60d27c32b0bb95daf12ea348b30ea5
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-10 17:53:00 -0700

    0c6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	double d;
 	char c;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a char: %u byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %u byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %u byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long long int: %u byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %u byte(s)\n", (unsigned long)sizeof(i));
 return (0);
 }
