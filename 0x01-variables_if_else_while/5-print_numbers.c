2a18cbecc6d5dcf69670567af7b512d2265b8c90
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-11 19:34:48 -0700

    1c5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,11 +7,12 @@
 int main(void)
 
 {
-        char i = '0';
-
-        while (i <= '9')
-{               putchar(i);
-        i++;
+char i = '0';
+	
+while (i <= '9')
+{
+putchar(i);
+i++;
 }
 putchar('\n');
 return (0);
