36ac83dc5976600b61771f94cf240392cc643536
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-11 18:22:08 -0700

    1c3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,19 +6,19 @@
  */
 int main(void)
 {
-	char alphabet = 'a';
-	char Alphabet = 'A';
+char alphabet = 'a';
+char Alphabet = 'A';
 
-	while (alphabet <= 'z')
-	{
-		putchar(alphabet);
-		alphabet++;
-	}
-	while (Alphabet <= 'Z')
-	{
-	 	putchar(Alphabet);
-		Alphabet++;
-	}
-	putchar('\n');
-    return (0);
+while (alphabet <= 'z')
+{
+putchar(alphabet);
+alphabet++;
+}
+while (Alphabet <= 'Z')
+{
+putchar(Alphabet);
+Alphabet++;
+}
+putchar('\n');
+return (0);
 }
