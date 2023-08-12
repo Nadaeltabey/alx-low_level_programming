41dd9bfbe6721f80309c17667844169b4edc082a
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-11 17:51:48 -0700

    1c3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char (alphabet);
+	char (Alphabet);
+
+	if (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	if (Alphabet = 'A'; Alphabet <= 'Z'; Alphabet++)
+	{
+	 	putchar(Alphabet);
+	}
+	putchar('\n');
+    return (0);
+}
