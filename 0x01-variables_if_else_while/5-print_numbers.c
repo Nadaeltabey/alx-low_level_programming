fed335e7d2d79985cc94668d094a358dac30c53a
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-11 19:21:48 -0700

    1c5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+        double i = '0';
+
+        while (i <= '9')
+{               printf("%f", i);
+        i++;
+}
+printf("\n");
+return (0);
+}
