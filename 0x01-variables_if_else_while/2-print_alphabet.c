d7ae0d0ec6c59c7ca923dc659469a6b3048559a7
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-11 17:36:27 -0700

    1c2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
 	char alphabet;
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {		putchar(alphabet);
 }
 putchar('\n');
-    return (0);
+return (0);
 }
