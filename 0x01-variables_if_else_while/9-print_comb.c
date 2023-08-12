7aa5ace9c11323d82b214de52ebd9324d8fc401e
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-12 06:18:25 -0700

    1c9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,15 @@ int main(void)
 {
 int i = '0';
 
-while (i < '9')
+while (i <= '9')
 {
 putchar(i);
+if (i == 9)
+break;
 putchar(',');
 putchar(' ');
 i++;
 }
-putchar('9');
+putchar('\n');
 return (0);
 }
