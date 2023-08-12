aac6f056e4c444cfd3f434bf2202a0ba0157f3c5
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-12 07:40:19 -0700

    1c4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,8 +11,7 @@ char alphabet = 'a';
 
 while (alphabet <= 'z')
 {
-if (alphabet != 'e' && alphabet != 'q'){
-continue;}
+if (alphabet != 'e' && alphabet != 'q')
 putchar(alphabet);
 alphabet++;
 }
