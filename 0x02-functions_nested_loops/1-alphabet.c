03354bcc76821dd94590a4bcd2842975e04a4d94
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-15 08:57:46 -0700

    3c1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+void print_alphabet(void);
+char i = 'a';
+while (i <= 'z')
+{
+_putchar(i);
+_putchar('\n');
+}
+return (0);
+}
