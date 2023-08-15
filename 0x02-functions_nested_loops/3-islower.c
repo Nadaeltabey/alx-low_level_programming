aeb2012d9d990e75f22dd3fa512ecac5fa07ebc1
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-15 16:16:51 -0700

    3c3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * _islower  - utilizes on the _putchar function to tell if
+ * the letter is lowercase
+*/
+int _islower(int c)
+
+{
+if (c >= 97 && c <= 122)
+{
+return(1);
+}
+else
+{
+return(0);
+}
+}
