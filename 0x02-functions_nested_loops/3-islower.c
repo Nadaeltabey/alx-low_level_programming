af1a721fe1b368aa16595af9edeece8de8e21f30
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-15 16:26:50 -0700

    3c3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,19 @@
 /**
  * _islower  - utilizes on the _putchar function to tell if
  * the letter is lowercase
+ * @c: operand
+ *
+ *Return:  1 (Success) and 0 (failure)
 */
 int _islower(int c)
 
 {
 if (c >= 97 && c <= 122)
 {
-return(1);
+return (1);
 }
 else
 {
-return(0);
+return (0);
 }
 }
