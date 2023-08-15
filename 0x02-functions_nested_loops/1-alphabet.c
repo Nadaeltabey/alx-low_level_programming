10a9b421c9959c30343a0855ff0611962f0bf30f
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-15 12:19:39 -0700

    3c2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,10 @@
 #include"main.h"
+
 /**
- *prints the alphabet in lowercase from a to z
- *lowercase
- *
- */
+ * print_alphabet - utilizes on the _putchar function to print
+ * the alphabet a - z
+*/
+
 void print_alphabet(void)
 
 {
