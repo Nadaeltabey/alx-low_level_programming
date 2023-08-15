873b4efe29beb37d1978095ad77dcc75c6eee51a
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-15 10:43:15 -0700

    1c1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,8 @@
 #include"main.h"
-void print_alphabet(void)
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ *prints the alphabet, in lowercase
  */
+void print_alphabet(void)
 
 {
 int i = 'a';
