e528078f355de7b779963b7f05f6caf8f16d8655
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-16 05:44:14 -0700

    3c10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,10 @@
 #include"main.h"
 
 /**
- * add - function to give the 
+ * add - function to give the
  * sum of two  numbers
- *
+ *@a : first operand
+ *@b : second operand
  *Return:  1 (Success) and 0 (failure)
 */
 int add(int a, int b)
