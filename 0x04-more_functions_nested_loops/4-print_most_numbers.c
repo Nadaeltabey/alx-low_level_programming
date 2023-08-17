6d88d61f192cfa312b2346dd8ca3f9ef5a1b3bab
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-17 13:25:38 -0700

    4c4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * print_numbers  - function to give the
+ * numbers except two and four
+ *
+ *
+ *Return:  1 (Success) and 0 (failure)
+*/
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+	if (i != 2 && i != 4)
+	{
+	_putchar(i + '0');
+	}
+	i++;
+	}
+_putchar('\n');
+}
