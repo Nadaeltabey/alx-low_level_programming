24d004630bb19968744e8824da877f14bb00f5f0
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-17 11:52:17 -0700

    4c2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * mul  - function to give the muliplication
+ * of two numbers
+ * @a: operand
+ * @b: operand
+ *
+ *Return:  1 (Success) and 0 (failure)
+*/
+int mul(int a, int b)
+{
+	/*int y = a * b;*/
+	return (a * b);
+}
