afd5b55d1bfa8e080232e11296f762ecbda6531b
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-17 11:04:20 -0700

    4c1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * isdigit  - function to tell if it is a
+ * digit
+ * @c: operand
+ *
+ *Return:  1 (Success) and 0 (failure)
+*/
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
