91b8d13477d05e84c6f6c8a24ba328e4da5b04b6
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-17 12:05:16 -0700

    4c3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * print_numbers  - function to tell if it is a
+ * letter of lowercase or uppercase
+ *
+ *Return:  1 (Success) and 0 (failure)
+*/
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i >= 9)
+		_putchar(i);
+	i++;
+}
