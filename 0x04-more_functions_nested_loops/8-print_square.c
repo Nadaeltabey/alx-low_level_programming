7e0df1f66370de5058e51c4ed257b6346dc1ae21
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-18 07:08:25 -0700

    4c8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,23 +10,22 @@ void print_square(int size)
 {
 int i, j;
 
-        if (size <= 0)
-        {
-        _putchar('\n');
-        }
-        else
-        {
-        for (i = 1; i <= size; i++)
-        {
-        for (j = 1; j <= size; j++)
-        {
-        _putchar('#');
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (i = 1; i <= size; i++)
+	{
+	for (j = 1; j <= size; j++)
+	{
+	_putchar('#');
 	}
 	if (i < size)
 	{
 	_putchar('\n');
 	}
-        }
-         _putchar('\n');
-        }
+	_putchar('\n');
+	}
 }
