645deb88b711687977365d8391388bb62d2439e7
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-18 07:00:53 -0700

    4c8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include"main.h"
+
+/**
+ * print_square - function to give
+ * a square
+ * @size: operand
+ *Return:  1 (Success) and 0 (failure)
+*/
+void print_square(int size)
+{
+int i, j;
+
+        if (size <= 0)
+        {
+        _putchar('\n');
+        }
+        else
+        {
+        for (i = 1; i <= size; i++)
+        {
+        for (j = 1; j <= size; j++)
+        {
+        _putchar('#');
+	}
+	if (i < size)
+	{
+	_putchar('\n');
+	}
+        }
+         _putchar('\n');
+        }
+}
