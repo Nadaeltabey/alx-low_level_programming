2f0fce621b981a4b470443a6756b4ca4cb519ff5
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-18 06:04:19 -0700

    4c7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,15 +10,14 @@ void print_diagonal(int n)
 {
 int i, j;
 
-
 	if (n <= 0)
 	{
-        _putchar('\n');
+	_putchar('\n');
 	}
-        else
+	else
 	{
-			for (i=1; i<=n; i++)
-			{
+		for (i=1; i<=n; i++)
+		{
 				if  (i >= 2)
 				{
 				for (j=1; j<i; j++)
