2f1a5a6a3126c2ad5bb303431da74e3cbcf1f352
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-18 22:37:31 -0700

    3c7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,13 +3,24 @@
 /**
  * print_last_digit  - function to give the last
  * digit of a number
+ *@n: operand
  *
  *Return:  1 (Success) and 0 (failure)
 */
 int print_last_digit(int n)
 {
 int l;
+
+if (n >= 0)
+{
 l = n % 10;
 _putchar(l + '0');
 return (l);
+}
+else
+{
+l = ((n % 10) * -1);
+_putchar(l + '0');
+return (l);
+}
 }
