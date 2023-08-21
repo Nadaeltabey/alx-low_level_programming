be902e22bf8b0bfefe5e2d19856c9a1512aba736
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-21 07:03:54 -0700

    4c1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,16 +5,13 @@
  *
  * @a: operand
  * @b: operand
- * @x: operand
- * @y: operand
+ *
  *Return:  1 (Success) and 0 (failure)
 */
 void swap_int(int *a, int *b)
 {
-int x, y;
-*a = &x;
-*b = &y;
-x = x - y;
-y = x + y;
-x = y - x;
+int x;
+x = *a;
+*a = *b;
+*b = x;
 }
