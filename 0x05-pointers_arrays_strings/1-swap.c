051933b49763d35c26cc21e51aa7a3a41a933d48
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-21 06:34:02 -0700

    4c1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * swap_int - function to swap values
+ *
+ * @a: operand
+ * @b: operand
+ *
+ *Return:  1 (Success) and 0 (failure)
+*/
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
