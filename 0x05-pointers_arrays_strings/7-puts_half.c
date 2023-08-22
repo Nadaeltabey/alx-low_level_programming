b62e51b19e354303be0589f0410a0ae28e76ad93
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-22 12:23:02 -0700

    4c7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,15 +8,23 @@
 */
 void puts_half(char *str)
 {
-	int i, n, c;
+	int i;
 
-	c = 0;
+	/*
+	 * c = 0;
+	 * */
 	for (i = 0; str[i] != '\0'; i++)
-		c++;
+		i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+	_putchar(str[i]);
+	}
+	/*
 	n = c / 2;
 	if ((c % 2) == 1)
 		n = ((c - 1) / 2);
 	for (i = n; str[i] != '\0'; i++)
 		_putchar(str[i]);
+	*/
 _putchar('\n');
 }
