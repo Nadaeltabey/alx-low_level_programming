1c38846534905be5df1f4494c0c9ec5424edc7de
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-23 22:29:52 -0700

    5c3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,15 +11,15 @@ int _strcmp(char *s1, char *s2)
 {
 int y, i;
 
-	i = 0;
-	while (s1[i] == s2[i])
-	{
-		if (s1[i] != '\0' && s2[i] != '\0')
-		{
-			return (0);
-		}
-		i++;
-	}
+i = 0;
+while (s1[i] == s2[i])
+{
+if (s1[i] != '\0' && s2[i] != '\0')
+{
+return (0);
+}
+i++;
+}
 y = s1[i] - s2[i];
 return (y);
 }
