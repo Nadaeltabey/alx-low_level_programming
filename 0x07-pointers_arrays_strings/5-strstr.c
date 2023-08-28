b1eda6fd0f1d2928cf54af91b8b7637e1a01f0c0
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-28 14:54:53 -0700

    7c5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,24 +3,24 @@
 /**
  * *_strstr - function to give
  * a mem
- * @*haystack: operand
- * @*needle: operand
+ * @haystack: operand
+ * @needle: operand
  *Return:  1 (Success) and 0 (failure)
 */
 char *_strstr(char *haystack, char *needle)
 {
-int i;
-int j;
+char *p = haystack;
+char *j = needle;
 
-for (i = 0; haystack[i] != '\0'; i++)
-		j = 0;
-		while (haystack[i] == needle[j] && needle[j] != '\0')
-		{
-			i++;
-			j++;
-		}
-			if (needle[j] == '\0')
+for (; *haystack != '\0'; haystack++)
+{
+	while (*p == *j && *j != '\0')
+	{
+	p++;
+	j++;
+	}
+			if (*j == '\0')
 			return (haystack);
-return ('\0');
 }
+return (0);
 }
