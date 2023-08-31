03acc7d2dfb8956b4a9f7feb07ccbc282ef03591
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-31 05:16:52 -0700

    8c0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,14 +4,15 @@
  * _puts_recursion - function to give
  * a string
  * @s: operand
- *Return:  1 (Success) and 0 (failure)
 */
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
-{
+	{
 	_putchar('\n');
-}
+	return;
+	}
 	_putchar(*s);
-	_puts_recursion(s + 1);
+	s++;
+	_puts_recursion(s);
 }
