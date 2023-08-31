a6fe32b52c5eb315d2725929db291f628a33b662
Nadaeltabey <nano.ahmed1@gmail.com>
2023-08-31 04:54:43 -0700

    8c0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function to give
+ * a string
+ * @s: operand
+ *Return:  1 (Success) and 0 (failure)
+*/
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+	_putchar('\n');
+}
+_putchar(*s);
+_puts_recursion(s + 1);
+}
