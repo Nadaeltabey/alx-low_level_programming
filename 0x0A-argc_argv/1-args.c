0d51912afdc014c9c081a2067d176b74898541dc
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-01 16:39:26 -0700

    Ac1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+/**
+ * main - writes th
+ * @argc: Th
+ * @argv: operand
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc);
+return (0);
+}
