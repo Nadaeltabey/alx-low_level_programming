217dec30d3c0993609261ddaace07d6e925da9f0
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-01 16:32:26 -0700

    Ac0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,13 @@
 /**
  * main - writes th
  * @argc: Th
- * @argb[]: operand
+ * @argv: operand
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 (void)argc;
-printf ("%s\n", argv[0]);
-return(0);
+printf("%s\n", argv[0]);
+return (0);
 }
