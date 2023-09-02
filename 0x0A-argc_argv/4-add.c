6c03b35dc65a38962b6913e4feba448cdcd5a6c4
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-01 19:23:43 -0700

    Ac4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "ctype.h"
+
+/**
+ * main - writes th
+ * @argc: Th
+ * @argv: operand
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(int argc, char *argv[])
+{
+int i, sum = 0;
+if (argc == 0)
+{
+printf("0\n");
+return (0);
+}
+else
+{
+for (i = 1; i < argc; i++)
+{
+if (!atoi(argv[i]))
+{
+printf("Error\n");
+return (1);
+}
+sum = sum + atoi(argv[i]);
+}
+}
+printf("%d\n", sum);
+return (0);
+}
