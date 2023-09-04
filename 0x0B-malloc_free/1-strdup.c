5829473a4028d0982cd520b3a54f398b563001d7
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-04 16:13:54 -0700

    9c1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include "stdlib.h"
+#include "stdio.h"
+
+/**
+ * *_strdup - function to give
+ * an array by malloc
+ * @str: operand
+ *Return:  1 (Success) and 0 (failure)
+*/
+char *_strdup(char *str)
+{
+unsigned int i;
+unsigned int m = 0;
+char *j;
+while (str)
+{
+m++;
+}
+j = (char *)malloc(m *sizeof(char));
+if (str == NULL || j == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < m; i++)
+{
+j[i] = str[i];
+}
+return (j);
+free(j);
+}
