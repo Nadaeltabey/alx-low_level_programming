1a7ffcb2ec79b025a0df3ca8c0b551c98f85fa89
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-11 16:56:22 -0700

    Ec0

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+/**
+ * init_dog - writes th
+ * struct dog _ writes
+ * @name: Th
+ * @float: operand
+ * @age: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d)
+{
+d.name = name;
+d.age = age;
+d.owner = owner;
+}
+}
