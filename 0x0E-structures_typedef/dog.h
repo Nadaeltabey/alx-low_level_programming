1481f060cdde9f8cbcbb7e6f60f5415e63a13a8a
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-11 16:29:06 -0700

    Ec0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - writes th
+ * @name: Th
+ * @age: operand
+ * @owner: Operand
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
