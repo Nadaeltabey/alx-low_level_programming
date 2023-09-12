12d9263b583a13b5d33357a387d5f52a5a053d85
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-11 17:36:37 -0700

    Ec2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,4 @@
-#include "stdio.h"
+#include <stdio.h>
 #include "dog.h"
 
 /**
@@ -9,7 +9,9 @@
 void print_dog(struct dog *d)
 {
 if (d)
-	printf("Name: $s\n", d->name ? d->name : "(nil)");
-	printf("Age: $f\n", d->age);
-	printf("Owner: $s\n", d->owner ? d->owner : "(nil)");
+{
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+}
 }
