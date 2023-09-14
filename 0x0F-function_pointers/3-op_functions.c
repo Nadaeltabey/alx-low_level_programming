5a997a42ac445c2774ac1a6091eade17aa1ff52a
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-13 19:04:14 -0700

    fc3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+/**
+ * op_add - writes th
+ * @a: Th
+ * @b: operand
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - writes th
+ * @a: Th
+ * @b: operand
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - writes th
+ * @a: Th
+ * @b: operand
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - writes th
+ * @a: Th
+ * @b: operand
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - writes th
+ * @a: Th
+ * @b: operand
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
