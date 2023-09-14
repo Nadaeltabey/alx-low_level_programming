e60eeddccb6a3121fe98185ad40253702d193974
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-14 15:33:48 -0700

    c2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+#include "stdio.h"
+#include "stdarg.h"
+/**
+ * print_stringss - writes th
+ * @n: Th
+ * @separator: operand
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int i;
+char *y;
+
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+        y = va_arg(args, char *);
+	if (y)
+	{
+        	printf("%s", y);
+        	if (separator && i < n - 1)
+        	{
+        	printf("%s", separator);
+        	}
+	}
+	else
+	{
+	printf ("nil");
+	}
+}
+va_end(args);
+printf("\n");
+}
