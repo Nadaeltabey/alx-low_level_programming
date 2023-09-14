5551bdb35a567fac8ac4b76df2ac54d398f8a515
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-13 19:34:21 -0700

    fc3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,21 +8,23 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-int (*op_func)(int, int), a, b;
+int num1, num2;
+char *op;
+
 if (argc != 4)
 {
 printf("Error\n"), exit(98);
 }
-a = atoi(argv[1]);
-b = atoi(argv[3]);
-op_func = argv[2];
-if (!op_func)
+num1 = atoi(argv[1]);
+op = argv[2];
+num2 = atoi(argv[3]);
+if (get_op_func(op) == NULL || op[1] != '\0')
 	printf("Error\n"), exit(99);
-if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	printf("Error\n"), exit(100);
-printf("%d\n", op_func(a, b));
+printf("%d\n", get_op_func(op)(num1, num2));
 return (0);
 
 }
