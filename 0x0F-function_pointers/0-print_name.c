5d26b8bb04af724ba14ed7a0bc48b56e92e28055
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-13 17:21:22 -0700

    fc0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * main - writes th
- * @argc: Th
- * @argv: operand
+ * print_name - writes th
+ * @name: Th
+ * @f: operand
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL || f == NULL)
-	return;
-f(name);
+if (name && f)
+	f(name);
 }
