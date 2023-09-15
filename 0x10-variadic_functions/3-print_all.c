6d3b9fc52229d3c5c872f6aa1d5396af175e40d5
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-14 17:21:18 -0700

    c3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,30 +1,50 @@
 #include "variadic_functions.h"
 #include "stdio.h"
 #include "stdarg.h"
-#include "ctype.h"
+
 /**
  * print_all - writes th
  * @format: Th
- * @separator: operand
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 void print_all(const char * const format, ...)
 {
 va_list args;
-unsigned int i, y, n;
-char *x;
-char z;
-float k;
-va_start(args, n);
+int i = 0;
+char *y, *separator = "";
 
-for (i = 0; i < n; i++)
-{	while (isdigit(y))
+va_start(args, format);
+
+if (format)
+{
+	while (format[i])
 	{
-        y = va_arg(args, int);
-	printf("%d%s",y, (separator ? separator : ""));
+		switch (format[i])
+		{
+			case'c';
+				printf("%s%c", separator, va_arg(args, int));
+				break;
+			case'i';
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case'f';
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case's';
+				y = va_arg(args, char *);
+				if (!y)
+					y = "(nil)";
+				 printf("%s%s", separator, y);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		separator = ", ";
+		i++;
 	}
-	while ()
-
-
+}
+printf("\n");
+va_end(args);
 }
