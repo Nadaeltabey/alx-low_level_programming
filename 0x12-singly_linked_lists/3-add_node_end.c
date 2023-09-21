0a7a5757afb1052e43a464b9724a64a44e838ae3
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-21 10:47:46 -0700

    12c2

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,22 +7,31 @@
  * @str: operand
  *Return:  1 (Success) and 0 (failure)
 */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-list_t *new;
+list_t *new, *ptr;
+ptr = *head;
 unsigned int c = 0;
 while (str[c])
 {
 c++;
 }
 new = malloc(sizeof(list_t));
-if (!new)
+if (!new || !head)
 {
 return (NULL);
 }
 new->str = strdup(str);
+if (!new->str)
+{
+free(new);
+return (NULL);
+}
 new->len = c;
-new->next = (*head);
-(*head) = new;
-return (*head);
+new->next = (NULL);
+while (ptr->next != NULL)
+{
+ptr = ptr->next;
+}
+ptr->next = new;
 }
