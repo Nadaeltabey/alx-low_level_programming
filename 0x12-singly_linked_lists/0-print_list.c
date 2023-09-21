a6ba5351af3f9d8c7c0469d06381c530121b538d
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-21 12:09:07 -0700

    12c0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,37 +1,20 @@
 #include "lists.h"
 
-/**
- * _strlen - function to give
- * alength
- * @s: operand
- *Return:  1 (Success) and 0 (failure)
-*/
-int _strlen(char *s)
-{
-int c = 0;
-if (!s)
-	return (0);
-while (*s++)
-{
-	c++;
-}
-	return (c);
-}
-
-/**
- * print_list - writes th
- * @h: Th
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 size_t print_list(const list_t *h)
 {
-size_t count = 0;
+int count = 0;
 while (h)
 {
-printf("[%d]%s\n", _strlen(h->str), h->str ? h->str : "(nil)");
-h = h->next;
+if (h->str == NULL)
+{
+printf("[0] (nill)\n");
+}
+else
+{
+printf("[%d]%s\n", h->len, h->str);
+}
 count++;
+h = h->next;
 }
 return (count);
 }
