c00befd907029bd00e62fc77a976a77a721d298d
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-21 16:19:43 -0700

    1c0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,18 +12,14 @@ size_t print_list(const list_t *h)
 {
 size_t count;
 count = 0;
-while (h != NULL)
+while (h)
 {
-if (h->str == NULL)
-{
-printf("[%d]%s\n", 0, "(nil)");
-}
-else
-{
-printf("[%d]%s\n", h->len, h->str);
-}
-h = h->next;
-count++;
+	if (!h->str)
+		printf("[%d]%s\n", 0, "(nil)");
+	else
+		printf("[%d]%s\n", h->len, h->str);
+	h = h->next;
+	count++;
 }
 return (count);
 }
