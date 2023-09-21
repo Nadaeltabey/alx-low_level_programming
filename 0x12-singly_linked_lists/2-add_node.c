bdfa14064b2b7b9c41d6fd4fc6f5296e38490709
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-21 10:23:09 -0700

    12c2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * _strlen - function to give
+ * alength
+ * @s: operand
+ *Return:  1 (Success) and 0 (failure)
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *new;
+unsigned int c = 0;
+while (str[c])
+{
+c++;
+}
+new = malloc(sizeof(list_t));
+if (!new)
+{
+return (NULL);
+}
+new->str = strdup(str);
+new->len = c;
+new->next = *head;
+*head = new;
+return (*head);
+}
