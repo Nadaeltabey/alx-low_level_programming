2281edec673acd11d25cf164dc0b0e82493228a0
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-21 15:29:13 -0700

    1c0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,12 +2,13 @@
 
 size_t print_list(const list_t *h)
 {
-size_t count = 0;
+size_t count;
+count = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
 {
-printf("[%d]%s\n", 0, "(nill)");
+printf("[%d]%s\n", 0, "(nil)");
 }
 else
 {
