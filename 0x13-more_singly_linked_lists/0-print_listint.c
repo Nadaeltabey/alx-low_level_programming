fc193ad3bfd9cb60ec63dd9f38e1b4b1eddedd13
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-26 04:40:44 -0700

    13c0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_listint - writes th
+ * @h: Th
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t c = 0;
+while (h)
+{
+	if (h->n)
+	{
+		printf("%d\n", n)
+		h = h->next;	
+	}
+c++;
+}
+return (c);
+}
