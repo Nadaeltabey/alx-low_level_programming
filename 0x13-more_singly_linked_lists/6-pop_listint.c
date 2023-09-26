c8be8e5a937c8b68b0ded277bedd29f8ab77dc65
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-26 12:43:40 -0700

    13c6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * list_len - writes th
+ * @h: Th
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int pop_listint(listint_t **head)
+{
+listint_t *temp;
+int s;
+if (!(*head) || !head)
+	return (0);
+s = *head->n;
+temp = *head;
+(*head) = (*head)->next;
+free(temp);
+return n;
+}
+
