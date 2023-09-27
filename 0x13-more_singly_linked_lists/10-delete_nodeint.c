32cce2044ec53fa8989cbcdd27d02ec2d0b6a8aa
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-26 17:45:12 -0700

    13c9

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - writes th
+ * @head: Th
+ * @index: operand
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+
+}
