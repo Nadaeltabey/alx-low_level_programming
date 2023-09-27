5fa742998e4b0f58081b16b272bc7b3fd8fc15bd
Nadaeltabey <nano.ahmed1@gmail.com>
2023-09-26 18:26:56 -0700

    13c10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 unsigned int i = 0;
 listint_t *last, *temp;
-last = malloc(sizeof(listint_t));
 
 if (!head || (*head))
 	return (-1);
